fc9064f21d8d8cc1ee40ab5a7d7386b655b7261c
TwilightSpar <1796748832@qq.com>
2020-03-31 16:55:45 -0700

    Test 10 UseStaticGenerationFunctionToCreateObject

--- src/Player.java
+++ src/Player.java
@@ -7,7 +7,7 @@ public class Player implements CanSumUpAbility {
     int additionAP;
     int additionMove;
     Champion mayorChampion;
-    public Player(String name, int additionHealth, int additionArmor, int additionAD, int additionAP, int additionMove, Champion mayorChampion) {
+    private  Player(String name, int additionHealth, int additionArmor, int additionAD, int additionAP, int additionMove, Champion mayorChampion) {
         this.name = name;
         this.additionHealth  = additionHealth;
         this.additionArmor   = additionArmor;
@@ -15,6 +15,10 @@ public class Player implements CanSumUpAbility {
         this.additionAP      = additionAP;
         this.additionMove    = additionMove;
         this.mayorChampion   = mayorChampion;
+    }
+
+    public static Player createPlayer(String name, int additionHealth, int additionArmor, int additionAD, int additionAP, int additionMove, Champion mayorChampion) {
+        return new Player(name, additionHealth, additionArmor, additionAD, additionAP, additionMove, mayorChampion);
     }
 
     public int ability(){
@@ -29,5 +33,17 @@ public class Player implements CanSumUpAbility {
         if (!other.mayorChampion.name.equals(this.mayorChampion.name))
             throw new IllegalArgumentException("Cannot compare player who plays " + this.mayorChampion.name + " to others who plays " + other.mayorChampion.name);
         return this.ability() > other.ability();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if(other == this) return true;
+        else if(!(other instanceof Player)) return false;
+        return  (this.additionHealth  == ((Player) other).additionHealth) &&
+                (this.additionArmor  == ((Player) other).additionArmor) &&
+                (this.additionAD     == ((Player) other).additionAD) &&
+                (this.additionAP == ((Player) other).additionAP) &&
+                (this.additionMove   == ((Player) other).additionMove) &&
+                (this.mayorChampion.name == ((Player) other).mayorChampion.name);
     }
 }
--- test/PlayerTest.java
+++ test/PlayerTest.java
@@ -7,8 +7,8 @@ public class PlayerTest {
     public void ZaZhiRivenIsBetterThanMe() {
         var riven = new Champion("riven", 0, 1, 2, -1, 3, ChampionType.WARRIOR);
 
-        var ZaZhi = new Player("ZhZhi", 2, 0, 3, 0, 3, riven);
-        var Me = new Player("Me", 0, 2, 4, 0, 0, riven);
+        var ZaZhi = Player.createPlayer("ZhZhi", 2, 0, 3, 0, 3, riven);
+        var Me = Player.createPlayer("Me", 0, 2, 4, 0, 0, riven);
         assertEquals(true, ZaZhi.isBetterThan(Me));
     }
     @Test(expected = IllegalArgumentException.class)
@@ -16,8 +16,8 @@ public class PlayerTest {
         var riven = new Champion("riven", 0, 1, 2, -1, 3, ChampionType.WARRIOR);
         var VN = new Champion("VN", 0, 1, 2, -1, 3, ChampionType.ADC);
 
-        var ZaZhi = new Player("ZhZhi", 2, 0, 3, 0, 3, riven);
-        var UZI = new Player("UZI", 0, 2, 4, 0, 0, VN);
+        var ZaZhi = Player.createPlayer("ZhZhi", 2, 0, 3, 0, 3, riven);
+        var UZI = Player.createPlayer("UZI", 0, 2, 4, 0, 0, VN);
         assertEquals(true, ZaZhi.isBetterThan(UZI));
     }
 
@@ -25,8 +25,17 @@ public class PlayerTest {
     public void ZaZhiAbilityIsBetterThanMe() {
         var riven = new Champion("riven", 0, 1, 2, -1, 3, ChampionType.WARRIOR);
 
-        var ZaZhi = new Player("ZhZhi", 2, 0, 3, 0, 3, riven);
-        var Me = new Player("Me", 0, 2, 4, 0, 0, riven);
+        var ZaZhi = Player.createPlayer("ZhZhi", 2, 0, 3, 0, 3, riven);
+        var Me = Player.createPlayer("Me", 0, 2, 4, 0, 0, riven);
         assertEquals(true, ZaZhi.ability() > Me.ability());
+    }
+
+    @Test
+    public void UseStaticGenerationFunctionToCreateObject() {
+        var riven = new Champion("riven", 0, 1, 2, -1, 3, ChampionType.WARRIOR);
+
+        var ZaZhi = Player.createPlayer("ZhZhi", 2, 0, 3, 0, 3, riven);
+        var Me = Player.createPlayer("Me", 0, 2, 4, 0, 0, riven);
+        assertEquals(false, ZaZhi.equals(Me));
     }
 }
